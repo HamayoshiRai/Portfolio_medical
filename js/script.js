6ba3f0fc2d7589f4b782373e4bc41fe33bcf7289
HamayoshiRai <148981804+HamayoshiRai@users.noreply.github.com>
2023-12-21 22:33:55 +0900

    slidebuttonの実装

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
-$(function(){
-  $('.btn-list li').click(function(){
+$(document).ready(function() {
+  var currentIndex = 0;
+  var slideCount = $('.slide img').length;
 
-    // hideで一度figure imgを消す
-    $("figure img").hide();
-
-    // src属性を今crickしたhref属性に入替える
-    $("figure img").attr("src", $(this).attr("href"));
-
-    // フェードインで表示
-    $("figure img").fadeIn(1000);
-
-    $('.active').removeClass('active');
-    $(this).addClass('active');
-
+  // ドットボタンのクリックイベント
+  $('.slide-dots li').click(function() {
+    var index = $(this).find('button').data('index');
+    changeSlide(index);
   });
+
+  // スライドを変更する関数
+  function changeSlide(index) {
+    currentIndex = index;
+
+    // すべてのスライドを非表示
+    $('.slide img').css('opacity', 0);
+
+    // クリックされたドットボタンに対応するスライドを表示
+    $('.slide img').eq(currentIndex).css('opacity', 1);
+
+    // ドットボタンのアクティブなスタイルの変更
+    $('.slide-dots li').removeClass('active');
+    $('.slide-dots li').eq(currentIndex).addClass('active');
+  }
+
+  // スライドショーの自動再生
+  setInterval(function() {
+    currentIndex = (currentIndex + 1) % slideCount;
+    changeSlide(currentIndex);
+  }, 5000);
 });
